e756f50c15679f92f4b8f39ff14d5476c3a07367
papa.bengu@touchlab.co.za <b1e2n3g4u5>
2015-04-02 12:31:49 +0200

    website inception code

---
+++ PosseCadre/PosseCadre/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PosseCadre.MvcApplication" Language="C#" %>
---
+++ PosseCadre/PosseCadre/Scripts/app.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="jquery-1.10.2.min.js" />
+/// <reference path="jquery-scrollto.js" />
+
+
+$(function () {
+
+    $scroll = $('#scroll');
+
+    $scroll.click(function () {
+        $('#about').ScrollTo({
+            duration: 2000,
+            easing: 'linear'
+        });
+    });
+
+});
