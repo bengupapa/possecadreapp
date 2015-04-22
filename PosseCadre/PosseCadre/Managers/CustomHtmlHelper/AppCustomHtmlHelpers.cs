710defd04b805752e3512a439b54d801f7fe70da
papa.bengu@touchlab.co.za <b1e2n3g4u5>
2015-04-22 13:26:00 +0200

    improved code logic. initial work on about us page

---
+++ PosseCadre/PosseCadre/Managers/CustomHtmlHelper/AppCustomHtmlHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PosseCadre.Managers.CustomHtmlHelper
+{
+    public static class AppCustomHtmlHelpers
+    {
+        public static IHtmlString Image(this HtmlHelper helper, string ImageName, string CssClassName) {
+
+            string location = "~/Resources/" + ImageName;
+
+            TagBuilder tag = new TagBuilder("img");
+            tag.Attributes.Add("src", VirtualPathUtility.ToAbsolute(location));
+            tag.AddCssClass(CssClassName);
+            return new MvcHtmlString(tag.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
